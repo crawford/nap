76124119d5e101cb3ffa0e21d9bec6d8d7d1dcb2
Alex Crawford <alex@acrawford.com>
2014-06-21 17:42:00 -0700

    Add tests for the default wrapper

--- nap_test.go
+++ nap_test.go
@@ -2,6 +2,7 @@ package nap
 
 import (
 	"net/http"
+	"reflect"
 	"testing"
 )
 
@@ -24,10 +25,79 @@ func TestDefaults(t *testing.T) {
 	} {
 		payload, status := test.fn(nil)
 		if payload != test.payload {
-			t.Fatalf("bad payload: got %q, want %q", payload, test.payload)
+			t.Fatalf("bad payload (%q): got %q, want %q", test.fn, payload, test.payload)
 		}
 		if status != test.status {
-			t.Fatalf("bad status: got %q, want %q", status, test.status)
+			t.Fatalf("bad status (%q): got %q, want %q", test.fn, status, test.status)
+		}
+	}
+}
+
+func TestDefaultWrapper(t *testing.T) {
+	for _, test := range []struct {
+		payload interface{}
+		status  Status
+		result  interface{}
+	}{
+		{
+			payload: nil,
+			status:  nil,
+			result: map[string]interface{}{
+				"status": map[string]interface{}{
+					"code":    http.StatusOK,
+					"message": "",
+				},
+				"result": nil,
+			},
+		},
+		{
+			payload: nil,
+			status:  OK{},
+			result: map[string]interface{}{
+				"status": map[string]interface{}{
+					"code":    http.StatusOK,
+					"message": "",
+				},
+				"result": nil,
+			},
+		},
+		{
+			payload: nil,
+			status:  OK{"test"},
+			result: map[string]interface{}{
+				"status": map[string]interface{}{
+					"code":    http.StatusOK,
+					"message": "test",
+				},
+				"result": nil,
+			},
+		},
+		{
+			payload: nil,
+			status:  NotFound{},
+			result: map[string]interface{}{
+				"status": map[string]interface{}{
+					"code":    http.StatusNotFound,
+					"message": "",
+				},
+				"result": nil,
+			},
+		},
+		{
+			payload: "test",
+			status:  nil,
+			result: map[string]interface{}{
+				"status": map[string]interface{}{
+					"code":    http.StatusOK,
+					"message": "",
+				},
+				"result": "test",
+			},
+		},
+	} {
+		result := DefaultWrapper{}.Wrap(test.payload, test.status)
+		if !reflect.DeepEqual(result, test.result) {
+			t.Fatalf("bad result (%q, %q): got %q, want %q", test.payload, test.status, result, test.result)
 		}
 	}
 }
